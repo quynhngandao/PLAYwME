9b7f4448c5c04447aefc2e48caaccaddd036b4f7
Quynh Ngan Dao <nguy2884@umn.edu>
2023-07-31 12:14:57 -0500

    added MUI theme

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-
+// MUI ThemeProvider for styling
+import { ThemeProvider } from '@mui/material/styles';
+import {theme} from './theme/theme'
 import store from './redux/store';
-
 import App from './components/App/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('react-root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={store}> 
+    <ThemeProvider theme={theme}>
       <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
