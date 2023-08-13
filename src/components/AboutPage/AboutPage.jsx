a9594777be80ea7329db7120ff5dd7e693ef8839
Quynh Ngan Dao <nguy2884@umn.edu>
2023-08-13 17:42:04 -0500

    added styling

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,10 +1,4 @@
-import {
-  Typography,
-  List,
-  ListItem,
-  Box,
-  Grid,
-} from "@mui/material";
+import { Typography, List, ListItem, Box, Grid } from "@mui/material";
 
 function AboutPage() {
   return (
@@ -19,7 +13,7 @@ function AboutPage() {
         </Typography>
 
         <Typography
-          sx={{ fontFamily: "varela round", fontWeight: 400 }}
+          sx={{ fontFamily: "varela round", textAlign: "center" ,fontWeight: 600, maxWidth:800, ml:2, p:2, pb:0 }}
           variant="body1"
         >
           PLAYwME: A platform to bridge the gap between animal lovers who want
@@ -29,24 +23,18 @@ function AboutPage() {
           can make a meaningful difference in the lives of these animals and
           increase their chances of finding loving homes.
         </Typography>
-        <Box justifyItems="center">
-        <Typography
-          sx={{ fontFamily: "varela round", fontWeight: 800 }}
-          variant="h3"
-          gutterBottom
-        >
-          Connect 
-        </Typography>
-       <img src="https://scontent-msp1-1.xx.fbcdn.net/v/t1.15752-9/365384761_1121952718763948_2083754579984283983_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=j5cRm39PUI4AX8pqhNU&_nc_ht=scontent-msp1-1.xx&oh=03_AdQtvrd-zp0oxvfJE1O1EG-Zton0QAkbRUQp7N4j6ii49g&oe=64FF6A34"/>
-     </Box>
+        <Box justifyItems="center" sx={{m:5}}>
+          <img width="200" height="50" src="https://hirechamp.com/wp-content/uploads/2017/04/LinkedIn-button-Connect-With-Me.jpg"/>
+          <br/>
+          <img src="https://scontent-msp1-1.xx.fbcdn.net/v/t1.15752-9/365384761_1121952718763948_2083754579984283983_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=j5cRm39PUI4AX8pqhNU&_nc_ht=scontent-msp1-1.xx&oh=03_AdQtvrd-zp0oxvfJE1O1EG-Zton0QAkbRUQp7N4j6ii49g&oe=64FF6A34" />
+        </Box>
       </Grid>
 
       <Grid item sx={{ textAlign: "center" }} xs={8}>
-
-      <Box item  textAlign="center" >
-      <Grid>
+        <Box item textAlign="center">
+          <Grid>
             <Typography
-              sx={{ fontFamily: "varela round", fontWeight: 800 }}
+              sx={{ fontFamily: "varela round", fontWeight: 800, p:1}}
               variant="h3"
               gutterBottom
             >
@@ -54,92 +42,85 @@ function AboutPage() {
             </Typography>
           </Grid>
           <Grid>
-          <List sx={{ m: 0, p: 0, textAlign: "center" }}>
-              <ListItem>
-                <Typography
-                  sx={{ fontFamily: "varela round", fontWeight: 500 }}
-                >
+            
+              <Box
+                sx={{ m: 0, p: 0, textAlign: "center", alignContent: "center" }}
+              >
+              <Typography sx={{m: 1, fontFamily: "varela round", fontWeight: 500 }}>
                   Scaling the project
                 </Typography>
-              </ListItem>
-              <ListItem>
-                <Typography
-                  sx={{ fontFamily: "varela round", fontWeight: 500 }}
-                >
+
+                <Typography sx={{m: 1, fontFamily: "varela round", fontWeight: 500 }}>
                   Allow pets owners to connect
                 </Typography>
-              </ListItem>
-            </List>
+              </Box>
+    
           </Grid>
         </Box>
 
-        <Box item  textAlign="center" >
+        <Box item textAlign="center">
           <Typography
-            sx={{ fontFamily: "varela round", fontWeight: 800 }}
+               sx={{ fontFamily: "varela round", fontWeight: 800, p:1}}
             variant="h3"
             gutterBottom
           >
             Technologies
           </Typography>
-          <List sx={{ m: 0, p: 0, textAlign: "center" }}>
-            <ListItem>
-              <Typography sx={{ fontFamily: "varela round", fontWeight: 500 }}>
-                React
-              </Typography>
-            </ListItem>
-            <ListItem>
-              <Typography sx={{ fontFamily: "varela round", fontWeight: 500 }}>
-                Redux/Redux-Saga
-              </Typography>
-            </ListItem>
-            <ListItem>
-              <Typography sx={{ fontFamily: "varela round", fontWeight: 500 }}>
-                Node/Express
-              </Typography>
-            </ListItem>
-            <ListItem>
-              <Typography sx={{ fontFamily: "varela round", fontWeight: 500 }}>
-                PostgreSQL
-              </Typography>
-            </ListItem>
-            <ListItem>
-              <Typography sx={{ fontFamily: "varela round", fontWeight: 500 }}>
-                Material UI
-              </Typography>
-            </ListItem>
-            <ListItem>
-              <Typography sx={{ fontFamily: "varela round", fontWeight: 500 }}>
-                RESTful API
-              </Typography>
-            </ListItem>
-          </List>
+
+          <Box sx={{ textAlign: "center", alignContent: "center" }}>
+            <Typography sx={{m: 1, fontFamily: "varela round", fontWeight: 500 }}>
+              React
+            </Typography>
+
+            <Typography sx={{m: 1, fontFamily: "varela round", fontWeight: 500 }}>
+              Redux/Redux-Saga
+            </Typography>
+
+         <Typography sx={{m: 1, fontFamily: "varela round", fontWeight: 500 }}>
+              Node/Express
+            </Typography>
+
+         <Typography sx={{m: 1, fontFamily: "varela round", fontWeight: 500 }}>
+              PostgreSQL
+            </Typography>
+
+         <Typography sx={{m: 1, fontFamily: "varela round", fontWeight: 500 }}>
+              Material UI
+            </Typography>
+
+         <Typography sx={{m: 1, fontFamily: "varela round", fontWeight: 500 }}>
+              RESTful API
+            </Typography>
+          </Box>
         </Box>
 
-        <Box item  textAlign="center" >
+        <Box item textAlign="center">
           <Typography
-            sx={{ fontFamily: "varela round", fontWeight: 800 }}
+             sx={{ fontFamily: "varela round", fontWeight: 800, p:1}}
             variant="h3"
             gutterBottom
           >
             Acknowledgment
           </Typography>
-          <List sx={{ m: 0, p: 0, textAlign: "center" }}>
-            <ListItem>
-              <Typography sx={{ fontFamily: "varela round", fontWeight: 500 }}>
+        
+          <Box
+                sx={{ textAlign: "center", alignContent: "center" }}
+              >
+           <Typography sx={{m: 1, fontFamily: "varela round", fontWeight: 500 }}>
                 Friends & Family{" "}
               </Typography>
-            </ListItem>
-            <ListItem>
-              <Typography sx={{ fontFamily: "varela round", fontWeight: 500 }}>
-                <b>EMERALD Cohort:</b> Emma & all my classmates
+        
+            
+           <Typography sx={{m: 1, fontFamily: "varela round", fontWeight: 500 }}>
+                <b>EMERALD Cohort:</b> My instructor & all my classmates
               </Typography>
-            </ListItem>
-            <ListItem>
-              <Typography sx={{ fontFamily: "varela round", fontWeight: 500 }}>
+        
+            
+           <Typography sx={{m: 1, fontFamily: "varela round", fontWeight: 500 }}>
                 ALL the wonderful Prime Staff
               </Typography>
-            </ListItem>
-          </List>
+        
+          </Box>
         </Box>
       </Grid>
     </Grid>
